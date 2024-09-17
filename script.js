a8e6d9d092c970e6cbc8978ae624935850344b07
OwenABC1 <owennsullivan@icloud.com>
2024-09-17 13:49:25 -0700

    Final Update for page 1

--- script.js
+++ script.js
@@ -1,6 +1,20 @@
-(document).ready(function() {
+$(document).ready(function() {
 
-	$("revealpassword").click(function(){
+	$("#revealpassword").click(function(){
 		$("#password").slideToggle("slow");
 	});
-}
+	
+	$(".incorect").click(function(){
+		$(".wrong").show();
+	});
+	$(".btn-close").click(function(){
+		$(".wrong").hide();
+	});
+	
+	 $(".Submit").click(function(){
+		if($("#pword").val() == $('#yes').val()){
+			window.location.href = 'page2.html'
+			return false;
+		}
+  });
+});
